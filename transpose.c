6627526203b8fa7ddde67b439124792a1261affd
Sargun  Singh <138251601+gluonparticle@users.noreply.github.com>
2024-02-06 06:46:15 +0000

    Update transpose.c

--- transpose.c
+++ transpose.c
@@ -1,48 +1,51 @@
-//program to find transpose of a matrix
-
-#include <stdio.h>
-
-int main()
+include<stdio.h>
+void main()
 {
-    int a[10][10], transpose[10][10], r, c, i, j;
+int a[20 ][20],b[20][20], m,n, i,j;
+printf(“enter the rows and column of matrix\n”);
+scanf(“%d%d”,&m,&n);
+printf(“Enter the elements of Matrix\n”);
+for(i=0;i<m;i++)
+{
+for(j=0;j<n;j++)
+{
+scanf(“%d”,&a[i][j]);
+}
+}
+printf(“Enter the elements of Matrix are\n”);
+for(i=0;i<m;i++)
+{
+for(j=0;j<n;j++)
+{
+printf(“%d\t”,a[i][j]);
+}
+printf(“\n”);
+}
+printf(“Matrix Transpose\n”);
+for(i=0;i<m;i++)
+{
+for(j=0;j<n;j++)
 
-    printf("Enter rows and columns of matrix: ");
-    scanf("%d %d", &r, &c);
+Availaible at VTU HUB (Android App)
 
-    printf("\nEnter elements of matrix:\n");
-    for (i = 0; i < r; ++i)
-        for (j = 0; j < c; ++i)
-        {
-            printf("Enter element a%d%d: ", i + 1, j + 1);
-            scanf("%d", &a[i][j]);
-        }
+Mangalore Institute of Technology and Engineering,
 
-    // Displaying the matrix a[][]
+Moodabidri. 22POP13
+22POP13
 
-    printf("\nEntered Matrix: \n");
-    for (i = 0; i < r; i++)
-        for (j = 0; j < c; j++)
-        {
-            printf("%d  ", a[i][j]);
+Department of Computer Science & Engineering 19
 
-        }
-
-    // Finding the transpose of matrix a
-    for (i = 0; i < r; i++)
-        for (j = 0; j < c; j++)
-        {
-            transpose[j][i] = a[i][j];
-        }
-
-    // Displaying the transpose of matrix a
-    printf("\nTranspose of Matrix:\n");
-    for (i = 0; i < c; ++i)
-        for (j = 0; j < r; ++j)
-        {
-            printf("%d  ", transpose[i][j]);
-            if (j == r - 1)
-                printf("\n");
-        }
-
-    return 0;
+{
+b[j][i]=a[i][j];
+}
+}
+printf(“The Transpose of the matrix is \n”);
+for(i=0;i<n;i++)
+{
+for(j=0;j<m;j++)
+{
+printf(“%d\t”,b[i][j]);
+}
+printf(“\n”);
+}
 }
